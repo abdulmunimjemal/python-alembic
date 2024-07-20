2e449194b6ca03dd422897897dd63bc651663dce
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-20 03:31:30 +0300

    [CLN] Example cleanup

--- src/app.py
+++ src/app.py
@@ -16,9 +16,9 @@ Session = sessionmaker(bind=engine)
 def app():
     with Session() as session:
         session.add_all([
-            Student(name="Alice", age=25),
-            Student(name="Bob", age=24),
-            Student(name="Charlie", age=23),
+            Student(name="Alice", age=25, note="Alice's notes"),
+            Student(name="Bob", age=24, note="Bob's notes"),
+            Student(name="Charlie", age=23, note="Charlie's notes"),
         ])
         session.commit()
     
