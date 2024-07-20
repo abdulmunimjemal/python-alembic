d207d39e016162a5ffd9813443d775c2589e8315
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-20 03:06:10 +0300

    [ENH] Added Students model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,18 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String, Integer, func, MetaData
 
-Base = declarative_base()
+Base = declarative_base(metadata=MetaData(schema='myschema'))
 metadata = Base.metadata
+
+class Student(Base):
+    __tablename__ = 'students'
+    # __table_args__ = {'schema': 'myschema'}
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60), unique=True, nullable=False)
+    age = Column(Integer, nullable=False)
+    note = Column(String(255))
+    created_at = Column(DateTime, default=func.now())
+    
+    def __repr__(self):
+        return f'<Student(name={self.name}, age={self.age})>'
