f80ca2000306301b95cb2bff1436e85a7bb673cd
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-07-20 03:05:49 +0300

    [CLN] Cleaned DB examples

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import select
+from sqlalchemy.orm import sessionmaker
+
+from models import Student
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+database_url = os.getenv("DB_URI")
+
+engine = create_engine(database_url)
+Session = sessionmaker(bind=engine)
+
+def app():
+    with Session() as session:
+        session.add_all([
+            Student(name="Alice", age=25),
+            Student(name="Bob", age=24),
+            Student(name="Charlie", age=23),
+        ])
+        session.commit()
+    
+    with engine.connect() as conn:
+        stmt = select(Student)
+        result = conn.execute(stmt)
+        students = result.fetchall()
+        print(students)
+
+if __name__ == "__main__":
+    app()
